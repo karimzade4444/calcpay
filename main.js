6bb93873190951815f1ce0b7c12cff50e5135c0e
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-04-15 19:33:06 +0500

    End

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
-let okl = Number(document.getElementById("okl").value);
-let prem = Number(document.getElementById("prem").value);
-let payday = Number(document.getElementById("payday").value);
-let paynight = Number(document.getElementById("paynight").value);
-let payceleb = Number(document.getElementById("payceleb").value);
-let payhours = Number(document.getElementById("payhours").value);
+window.onload = () => {
+  let result = document.querySelector(".result");
 
-let partpay = Number(document.getElementById("partpay").value);
-let pay = Number(document.getElementById("pay").value);
-let fullpay = Number(document.getElementById("fullpay").value);
-let fullpaytax = Number(document.getElementById("fullpaytax").value);
-let tax = Number(document.getElementById("tax").value);
-let result = document.querySelector(".result");
+  result.onclick = () => {
 
-let index = 78;
-let phys = 0.12;
-let social = 0.02;
+    let okl = Number(document.getElementById("okl").value);
+    let prem = Number(document.getElementById("prem").value);
+    let payday = Number(document.getElementById("payday").value);
+    let paynight = Number(document.getElementById("paynight").value);
+    let payceleb = Number(document.getElementById("payceleb").value);
+    let payhours = Number(document.getElementById("payhours").value);
 
-let paypart = okl * 0.4;
-let premtax = prem * 0.86;
+    let partpay = document.getElementById("partpay");
+    let pay = document.getElementById("pay");
+    let fullpay = document.getElementById("fullpay");
+    let fullpaytax = document.getElementById("fullpaytax");
+    let tax = document.getElementById("tax");
 
-let pp = paypart + premtax;
+    // проверка В САМОМ НАЧАЛЕ
+    if (!okl || !payhours) {
+      alert("Заполни обязательные поля!");
+      return;
+    }
 
+
+let paypart = okl*0.4;
+let premtax = prem *0.86;
+let pp = paypart+premtax;
 let hourspay = okl / payhours;
-let res = hourspay * payday * paynight * payceleb;
-let resultpay = res - res * social - (res - index * 2) * phys;
+let resday = hourspay * payday;
+let resnight = hourspay*paynight;
+let resceleb = hourspay*payceleb;
+let resall = resday+resnight+resceleb;
+let taxtwo = resall*0.02;
+let taxtwelve = ((resall-taxtwo)-156)*0.12;
+let resultpay =resall-taxtwo-taxtwelve;
+let rpwopp = resultpay-paypart
+let restax = resall-resultpay
+let res = resall
 
-let rpwopp = resultpay - paypart;
 
-let restax = res - resultpay;
 
-result.onclick = () => {
-  partpay.value = paypart;
-  pay.value = rpwopp;
-  fullpay.value = resultpay;
-  fullpaytax.value = res;
-  tax.value = restax;
-};
+    partpay.value=pp.toFixed(2);
+    pay.value=rpwopp.toFixed(2);
+    fullpay.value=resultpay.toFixed(2);
+    fullpaytax.value=res.toFixed(2);
+    tax.value=restax.toFixed(2);
+  
+}}
