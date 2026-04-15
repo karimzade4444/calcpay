36d0d68b4b0a5ddb658549ddb5557a9a732522d0
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-04-15 18:58:32 +0500

    onclick

--- main.js
+++ main.js
@@ -10,27 +10,29 @@ let pay = Number(document.getElementById("pay").value);
 let fullpay = Number(document.getElementById("fullpay").value);
 let fullpaytax = Number(document.getElementById("fullpaytax").value);
 let tax = Number(document.getElementById("tax").value);
-let result = document.querySelector(".result")
+let result = document.querySelector(".result");
 
 let index = 78;
 let phys = 0.12;
 let social = 0.02;
 
-let paypart = okl*0.4;
-let premtax = prem *0.86;
+let paypart = okl * 0.4;
+let premtax = prem * 0.86;
 
-let pp = paypart+premtax;
+let pp = paypart + premtax;
 
 let hourspay = okl / payhours;
 let res = hourspay * payday * paynight * payceleb;
-let resultpay = (res-(res*social))-((res-index*2)*phys);
+let resultpay = res - res * social - (res - index * 2) * phys;
 
-let rpwopp = resultpay-paypart
+let rpwopp = resultpay - paypart;
 
-let restax = res-resultpay;
+let restax = res - resultpay;
 
-
-
-
-
-
+result.onclick = () => {
+  partpay.value = paypart;
+  pay.value = rpwopp;
+  fullpay.value = resultpay;
+  fullpaytax.value = res;
+  tax.value = restax;
+};
