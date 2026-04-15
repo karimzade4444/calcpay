d580e9fca2bfcfcd8a86a2caf71e7af4db7f2249
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-04-15 18:34:27 +0500

    InpINform

---
+++ main.js
@@ -0,0 +1,10 @@
+function calculate(){
+    let okl = Number(document.getElementById("okl").value)
+    let prem = Number(document.getElementById("prem").value)
+    let payday  = Number(document.getElementById("payday").value)
+    let paynight = Number(document.getElementById("paynight").value)
+    let payceleb = Number(document.getElementById("payceleb").value)
+    let payhours = Number(document.getElementById("payhours").value)
+
+
+}
